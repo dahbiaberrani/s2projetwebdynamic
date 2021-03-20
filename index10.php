0d1f1f8e76594130081c9b7601f3c0744f2cc9f6
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-03-20 21:24:33 +0100

    modif index10

--- index10.php
+++ index10.php
@@ -35,12 +35,12 @@
                         echo("Désolé, accès à la base  impossible\n");
                         exit;
                     }
-                    mysqli_query("SET NAMES 'utf8'");
+                    mysqli_set_charset($connexion, "utf8");
              // Récupération des recettes 
               $requette="SELECT Nomrecette,Imagepath,Etapes FROM Recettes  ";      
-              $requette1="SELECT Nomingredient FROM Ingredients  "; 
-              $requette="SELECT Quantitee FROM Compostions Where Idrecette=1"; 
-              $requette2="SELECT commentaire FROM Commentaires ";
+               
+              $requette2="SELECT Quantitee FROM Compositions Where Idrecette=1";
+              
               $table_resultat =  mysqli_query($connexion,$requette);
             // affichage chaque recettes
             if($table_resultat){
@@ -57,25 +57,42 @@
                 echo"message de mysqli:".mysqli_error($connexion);
             }
 
-                // affichage chaque Ingrediens 
-            $table_resultat =  mysqli_query($connexion,$requette1);
-
-                      
+                // __________________________affichage chaque Ingrediens 
+            $requette1="SELECT Nomingredient FROM Ingredients  ";
+            $table_resultat =  mysqli_query($connexion,$requette1);   
             if($table_resultat){
-                echo ("Ingredients :<br>");
+                echo ("Ingredients de recettes  ");
 
                 while($ligne=mysqli_fetch_object($table_resultat))
-           
+            
                 {
-                echo ("<h1>".$ligne->Nomringredient."</h1><img src=".$ligne->Imagepath."><br>");
+                echo ("<p>".$ligne->Nomingredient."</p>");
                 }
-
+    
             }else{
                 echo "<p>Erreur dans l'exécution de la requette</p>";
                 echo"message de mysqli:".mysqli_error($connexion);
             }
 
-                mysqli_close($connexion);
+
+                 // __________________________affichage chaque Quantitée recette  
+            $requette3="SELECT commentaire FROM Commentaires ";
+            $table_resultat =  mysqli_query($connexion,$requette3);   
+            if($table_resultat){
+                echo ("Commentaire de la recette  crèpe salée ");
+                
+                while($ligne=mysqli_fetch_object($table_resultat))
+                            
+                {
+                echo ("<p>".$ligne->commentaire."</p>");
+                }
+                    
+                }else{
+                echo "<p>Erreur dans l'exécution de la requette</p>";
+                echo"message de mysqli:".mysqli_error($connexion);
+            }
+           
+            mysqli_close($connexion);
                 
 
         ?>
