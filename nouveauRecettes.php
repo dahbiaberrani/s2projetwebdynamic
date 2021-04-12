3f8f67ddfaa0882b6b7893937f55fb76d89f41d8
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-04-12 21:36:09 +0200

    avancer sur le formulaire d'ajout recettes

--- nouveauRecettes.php
+++ nouveauRecettes.php
@@ -1,89 +1,169 @@
+<?php  
+   echo phpversion();
+
+/**
+* @return bool
+*/
+function is_session_started()
+{
+    if ( php_sapi_name() !== 'cli' ) {
+        if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+            return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+        } else {
+            return session_id() === '' ? FALSE : TRUE;
+        }
+    }
+    return FALSE;
+}
+
+// Ouverture d'une session
+if ( is_session_started() === FALSE ){
+    session_start();
+    echo "ouvrture de session N°: </br>";
+    echo session_id();
+} 
+?>
+
+
+
 <html>
 
 <head>
-<title>Choix d'une opération sur 2 nombres</title>
+
 </head>
 
  <body>
-   <h2>Ajouter des nouveau Films</h2>
-  <form action="insererFilms.php" method="POST">
 
-	
-	   <br/>
+   <h2>Ajouter des nouveau recettes</h2></br>
 
-    <label for="real">Réalisateur</label>
+  <form action="nouveauRecettes.php" method="POST">
+    <!-- Nom recette -->
+        <label for="Nom">Nom Recette</label>
+        <input  id="Nom" name="Nom" type="text"> </br>
 
-   <select  id="real" name="real" > 
+    <!-- choix Catégorie recette -->
+         <label for="categorie">catégorie:</label> 
+   <select  id="categorie" name="Catégorie" type="text"> 
 <?php 
+
     
     $connexion=mysqli_connect('mi-mariadb.univ-tlse2.fr','dahbia.berrani-eps-h','Akbou_2021');
     if (!$connexion) {
         echo("Desolé, connexion au serveur impossible\n");
         exit;
-      }
-    if (!mysqli_select_db($connexion,'19_L1M_dahbia_berrani_eps_haddad')) {
+    }
+    if (!mysqli_select_db($connexion,'20_L2M_dahbia_berrani_eps_haddad')) {
         echo("Désolé, accès à la base  impossible\n");
         exit;
     }
     mysqli_set_charset($connexion, "utf8");
-    $requette1=("SELECT NumInd , Nom,Prenom From Individus");      
+    $requette1=("SELECT  Nomcategorie From Categories");      
     $resultat =  mysqli_query($connexion,$requette1);
-        if($resultat){
-            
-            while($ligne=mysqli_fetch_object($resultat)){
-    
-                echo ("<option value=\"".$ligne->NumInd."\"> ".$ligne->Prenom ." ".$ligne->Nom ."</option>\n");
-            }
-        }else{
-            echo "<p>Erreur dans l'exécution de la requette</p>";
-            echo"message de mysqli:".mysqli_error($connexion);
+    if($resultat){
+        
+        while($ligne=mysqli_fetch_object($resultat)){
+
+            echo ("<option value=\"".$ligne->Nomcategorie."\"> ".$ligne->Nomcategorie ."</option>\n");
         }
+    }else{
+        echo "<p>Erreur dans l'exécution de la requette</p>";
+        echo"message de mysqli:".mysqli_error($connexion);
+    }
 ?>
 </select></br>
+     <!-- ajouter nombre de personne  -->
+    <label for="Nombrepersonne ">Nombre personne </label>
+    <input  id="Nombrepersonnenre" name="Nombrepersonne" type="number"> </br>
 
-   <label for="titre">Titre du film</label>
 
-   <input  id="titre" name="titre" type="text"> </br>
-    <label for="genre ">Genre</label>
+       <!-- selectionner les ingredient de recettes  -->
 
-   <select  id="genre" name="genre" type="text">
-   <?php 
+       <label for="NomIngredient">Nom Ingredient</label>
+        <select  id="NomIngredient" name="NomIngredient" type="text" > </br> 
+        <option value=" " ></option></br>
+
+<?php 
+   
     $connexion=mysqli_connect('mi-mariadb.univ-tlse2.fr','dahbia.berrani-eps-h','Akbou_2021');
     if (!$connexion) {
         echo("Desolé, connexion au serveur impossible\n");
         exit;
       }
-    if (!mysqli_select_db($connexion,'19_L1M_dahbia_berrani_eps_haddad')) {
+    if (!mysqli_select_db($connexion,'20_L2M_dahbia_berrani_eps_haddad')) {
         echo("Désolé, accès à la base  impossible\n");
         exit;
     }
     mysqli_set_charset($connexion, "utf8");
-    $requette2=("SELECT Genre From Films GROUP by Genre");      
+    $requette2=("SELECT  Nomingredient From Ingredients");      
     $resultat =  mysqli_query($connexion,$requette2);
+   
         if($resultat){
-            
+        
             while($ligne=mysqli_fetch_object($resultat)){
     
-                echo ("<option value=\"".$ligne->Genre."\">".$ligne->Genre."</option>\n");
+             echo ("<option value=\"".$ligne->Nomingredient."\">  ".$ligne->Nomingredient . "</option>\n");
             }
-           
         }else{
             echo "<p>Erreur dans l'exécution de la requette</p>";
             echo"message de mysqli:".mysqli_error($connexion);
         }
-    
+
+
 ?>
-   </select> </br>
-  
-   
-   <label for="annee">Année de Réalisation</label>
+</select>
+            <!-- ajouter la quantite   -->
+    <label for="Quantite">Quantite </label>
+    <input  id="Quantite" name="Quantite" type="numbre" >
 
-   <input  id="annee" name="annee" type="number"></br>
+    <!-- selctionner unite -->
+    <label for="unite"> unité </label>
+    <select  id="unite" name="unite" type="text" > 
+        <option value="g" name="g" type="text">g</option>
+        <option value="ml" name="ml" type="text">ml</option>
+        <option value=" " name="sans unite" type="text" >unité</option>
+    </select></br>
+        <input  type="submit"  value="ajouter" name="Valider"/> 
+        <input type="reset" name="btAnnuler" value="Annuler" /></p>
 
-	 
-	<input value="ajouter" type="submit"/> 
+<?php
+    $unite = $_POST['unite'];
+    $quantite = $_POST['Quantite'];
+        if(!isset($_SESSION["mes_ingredients"])){
+            $_SESSION["mes_ingredients"] = array() ;
+        }
+ 
+        if (isset($_POST['Valider'])){
+            $nom = $_POST['NomIngredient'];
+            $_SESSION["mes_ingredients"]+=array($nom=>$quantite);
+            echo"ingredient ajouter à la recette: </br> ";
+         
+           //echo $_SESSION["mes_ingredients"];
+           // loop through the session array with foreach
+           echo"<ul>";
+            foreach($_SESSION["mes_ingredients"] as $key=>$quantite){
+                echo"<li>";
+                // and print out the values
+                echo $key.":".$quantite."".$unite;
+                
+                echo"</li>";
+            }
+            echo"</ul>";
+        } 
+?>
+
+
+
+     <!-- ajouter etapes de prepartion recette  -->
+   <label for="etapes">Etapes de preparation</label></br>
+    <textarea  id="etapes" name="etapes" cols="50" rows="20"> </textarea></br>  
+
+	
+        <input value="ajouter la recette" type="submit"/> 
 	<input type="reset" name="btAnnuler" value="Annuler" /></p>
-   </form>
+    </form>
+
+
+
  </body>
 
 </html>
