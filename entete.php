057147210a47707491eaa449b3b89fdaeaca5f93
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-05-02 14:23:37 +0200

    rebased working

--- entete.php
+++ entete.php
@@ -8,7 +8,7 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <header>
-
+   <h1>Rendez-vous dans l'assiette</h1>
    <nav id="menu">
       <ul>
          <li>
